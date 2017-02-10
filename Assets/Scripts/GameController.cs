879b4c75192d903faf99861f487d867b958d4f8f
Carsten Moberg Hammer <cm@cego.dk>
2017-02-10 14:02:26 +0100

    First edition test

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+	private int score = 0;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOver && Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	public void BirdScored() {
+		if(!gameOver) {
+			score++;
+			scoreText.text = "Score: " + score.ToString();
+		}
+	}
+	
+	public void BirdDied() {
+		gameOverText.SetActive(true);
+		gameOver = true;
+	}
+}
